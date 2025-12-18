dca077e1c5d9b641644be4c110fe498d80021308
199adarsh <raosahebherwade123@gmail.com>
2025-12-18 17:14:25 +0530

    Update

--- Sunbeam/D3_02.cpp
+++ Sunbeam/D3_02.cpp
@@ -17,7 +17,7 @@ int main() {
     cout << "Investment B: 6000 rs with 12% annual return\n\n";
     
     // Find years where Investment B profit exceeds Investment A profit
-    for (int years = 1; years <= 50; years++) {
+    for (int years = 0; years <= 50; years++) {
         double profitA = (profit_a * years) + profitA_final;
         double amountB = amount * pow(1 + returnRate, years);
         double profitB = amountB - amount;
