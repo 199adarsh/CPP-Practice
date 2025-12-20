c835983bea528ac6e367f0d5bea70fbc074189ac
199adarsh <raosahebherwade123@gmail.com>
2025-12-20 15:48:38 +0530

    Update

---
+++ Sunbeam/D4_07.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+struct ddmm {
+   
+    int dd;
+    int mm;
+    int yy;
+    
+    ddmm(int d, int m, int y) {
+        dd = d;
+        mm = m;
+        yy = y;
+    }
+    
+    void accept() {
+        cout << "Accept Date" << endl;
+        cout << "Date :" << endl;
+        cin >> dd;
+        cout << "Month :" << endl;
+        cin >> mm;
+        cout << "Year :" << endl;
+        cin >> yy;
+        if (yy < 1900)
+            cout << "loop gear" << endl;
+    }
+    
+    void print() {
+        cout << "Date :" << dd << endl;
+        cout << "Month :" << mm << endl;
+        cout << "Year :" << yy << endl;
+    
+    }
+    void init() {
+        dd = 01;
+        mm = 01;
+        yy = 2001;
+    }
+};
+
+int main() {
+    ddmm date(10, 8, 2008);
+
+    int choise ;
+    cout <<  "Enter your choise \n1. Print \n2. Accept \n3.Init" << endl;
+    cin >> choise;
+    switch (choise)
+    {
+    case 1:
+         date.print();
+        break;
+    case 2: date.accept();
+        break;
+    case 3:date.init();
+        break;
+        
+    
+    default:
+        break;
+    }
+   
+   
+
+    return 0;
+}
