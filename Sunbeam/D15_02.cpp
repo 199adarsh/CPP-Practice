21a90b8f6b07b4c204f37cfcdb9533e56425bb8a
199adarsh <raosahebherwade123@gmail.com>
2026-01-05 15:43:59 +0530

    Update

--- Sunbeam/D15_02.cpp
+++ Sunbeam/D15_02.cpp
@@ -8,46 +8,38 @@ class Bank {
         double balance;
         double value;
     public: 
-
         void accountCreation(){
-            try{
+           
             cout << "Enter the account no : " ;
             cin >>accNo;
             cout << "Enter the initial money to deposit : ";
             cin >> balance;
-            if (balance < 2000) throw string ("Initial balance cant be less than 2000 ");
+            if (balance < 2000) throw string ("Initial balance cant be less than 2000");
 
-            cout << "Account Created with Account No : " << accNo << " Balance : " << balance << endl;}
-            catch(string &e){
-            cout  << e << '\n';
-            }
+            cout << "Account Created with Account No : " << accNo << " Balance : " << balance << endl;
+
 
         }
 
         void deposit (){
-        try{
+
             cout << "Enter the money to deposit : ";
             cin >> value;
-            if (value <= 0) throw string (" Deposit cant be less than 0 ");
+            if (value <= 0) throw string ("Deposit cant be less than 0 ");
             else balance += value;
-            cout << "Money deposited to Account No : " << accNo << " Balance : " << balance << endl;}
-            catch(string &e){
-            cout  << e << '\n';
-            }
+            cout << "Money deposited to Account No : " << accNo << " Balance : " << balance << endl;
+            
         }
 
         void withdraw (){
-         try{   
+
             cout << "Enter the money to Withdraw : ";
             cin >> value;
             if (value >= (balance-2000)) throw string ("Please maintain Min Balance of 2000");
             else balance -= value;
-            cout << "Money Withdrawn to Account No : " << accNo << " Balance : " << balance << endl;}
-            catch(string &e){
-            cout  << e << '\n';
-            }
+            cout << "Money Withdrawn to Account No : " << accNo << " Balance : " << balance << endl;
+            
         }
-
 
 };
 
@@ -55,39 +47,44 @@ int main()
 {
     Bank b1;
     int choice;
-    b1.accountCreation();
+    try
+    {
+        b1.accountCreation();
+    }
+    catch(string &e)
+    {
+        cout  << e << '\n';
+        return 0;
+    }
       
     while(choice != 3){
         try
         {
-            
             cout << "Menue :" << endl;
-            cout << "1. Deposit" << endl;
+            cout << "1. deposit" << endl;
             cout << "2. Withdraw" << endl;
             cout << "3. Exit " << endl;
             cin >> choice;
-            if (choice > 2) throw string ("Invalid Choice !") ;
+            switch(choice){
+
+                case 1 :
+                    b1.deposit();
+                    break;
+                case 2 :
+                    b1.withdraw();
+                    break;
+                case 3 :
+                    cout << "Exited the Program";
+                    return 0 ;
+                default :
+                    cout << "Invalid Choice !"<< endl;                
+            }
         }
         catch(string &e)
         {
             cout  << e << '\n';
+            continue;
         }
-
-        switch(choice){
-
-            case 1 :
-                b1.deposit();
-                break;
-            case 2 :
-                b1.withdraw();
-                break;
-            case 0 : 
-                return 0 ;
-
-            default :
-                cout << "Invalid Choice !"<< endl;                
-            }
-
     }
     
     return 0;
