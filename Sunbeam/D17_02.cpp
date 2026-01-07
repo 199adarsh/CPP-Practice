fa0026aec604f2696d01baa3ffac679e5d10334f
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-01-07 15:40:34 +0530

    Update

--- Sunbeam/D17_02.cpp
+++ Sunbeam/D17_02.cpp
@@ -1,37 +1,49 @@
 #include <iostream>
 #include <map>
+#include <string>
 using namespace std;
 
 int main() {
     map<string, string> addressBook;
     string name, address;
 
+    cout<<"Enter 5 names and addresses: "<<endl;
     for (int i = 1; i <= 5; i++) {
-        cout<<"Name: "<<endl; getline(cin,name);
-        cout<<"Address: "<<endl; getline(cin, address);
+        cout <<"BOOK : "<<i<<endl;
+        cout<<"Name: "; getline(cin, name);
+        cout<<"Address: "; getline(cin, address);
         addressBook[name] = address;
+        cout <<"\n";
     }
 
-    cout<<"Address Book "<<endl;
 
-    for (auto &entry : addressBook) {
+
+    cout<<"\nDisplay Address Book: "<<endl;
+    for (const auto &entry : addressBook) {
         cout<<"Name: "<<entry.first<<endl;
         cout<<"Address: "<<entry.second<<endl;
+        cout <<"\n";
     }
 
-    cout<<"Enter name to delete: "<<endl;
+
+
+
+    cout<<"\nEnter name to delete: "<<endl;
     cin>>name;
 
-    if (addressBook.erase(name))
+    if (addressBook.erase(name) == 1)
         cout<<"Entry deleted!"<<endl;
     else
         cout<<"Name not found!"<<endl;
 
-    cout<<"Address Book After Deletion "<<endl;
-    
-    for (auto &entry : addressBook) {
+
+
+    cout<<"Address Book After Deletion: "<<endl;
+    for (const auto &entry : addressBook) {
+        
         cout<<"Name: "<<entry.first<<endl;
         cout<<"Address: "<<entry.second<<endl;
+        cout <<"\n";
     }
 
     return 0;
