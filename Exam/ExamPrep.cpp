3688f9058d210940df25c3f1437cf3bd76c52384
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-01-09 13:58:49 +0530

    Update

--- Exam/ExamPrep.cpp
+++ Exam/ExamPrep.cpp
@@ -7,4 +7,10 @@ int main()
     for (int j =0; j <10 ; j++ )
     cout << "Hellpw cpp";
     return 0;
+    /*umerating objects: 7, done.
+Counting objects: 100% (7/7), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (4/4), 430 bytes | 430.00 KiB/s, done.
+Total 4 (delta 1), reused 0 (delta 0), pack-reused */
 }
