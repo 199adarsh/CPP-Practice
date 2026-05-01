313c9ad1acfabbed4f0bb8e051e6e4f114d5dd8c
Adarsh Herwade <adarshherwade11214@gmail.com>
2026-05-01 20:47:46 +0530

    Micro Projects

---
+++ DAA Experiements/12 MicroProject01.cpp
@@ -0,0 +1,24 @@
+//Plagiarism Detection (LCS)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lcs(string a, string b) {
+    int m = a.length(), n = b.length();
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(a[i-1] == b[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+int main() {
+    cout << lcs("abcde", "ace"); // 3
+}
---
+++ DAA Experiements/12 MicroProject02.cpp
@@ -0,0 +1,27 @@
+//Hospital Bed Allocation (0/1 Knapsack)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsack(vector<int> wt, vector<int> val, int W) {
+    int n = wt.size();
+    vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+
+    for(int i=1;i<=n;i++){
+        for(int w=1;w<=W;w++){
+            if(wt[i-1] <= w)
+                dp[i][w] = max(val[i-1] + dp[i-1][w-wt[i-1]],
+                               dp[i-1][w]);
+            else
+                dp[i][w] = dp[i-1][w];
+        }
+    }
+    return dp[n][W];
+}
+
+int main() {
+    vector<int> wt = {1,2,3};
+    vector<int> val = {10,15,40};
+    cout << knapsack(wt, val, 5); // 55
+}
