332c84b5c1793f68795f3ae717ad39068116ece6
Adarsh Herwade <adarshherwade11214@gmail.com>
2026-06-06 19:27:13 +0530

    Update

--- DAA Experiements/07 Knapsack.cpp
+++ DAA Experiements/07 Knapsack.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    vector<int> val = {20, 20, 50};
-    vector<int> wt  = {2, 3, 4};
+    int val[] = {20, 60, 70};
+    int wt[]  = {2, 3, 4};
 
-    int n = val.size();
+    int n = 3;
     int W = 5;
 
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    int dp[10][10] = {0};
 
     for (int i = 1; i <= n; i++) {
         for (int cap = 0; cap <= W; cap++) {
 
             if (wt[i - 1] <= cap)
-                                dp[i][cap] = max(   dp[i - 1][cap],
-                                                    val[i - 1] + dp[i - 1][cap - wt[i - 1]]);
-                                            
+                dp[i][cap]=max( dp[i - 1][cap],  val[i - 1] + dp[i - 1][cap - wt[i - 1]]);
+
             else
                 dp[i][cap] = dp[i - 1][cap];
-            
-            
         }
     }
 
--- DAA Experiements/08 Largest Subseq.cpp
+++ DAA Experiements/08 Largest Subseq.cpp
@@ -3,27 +3,20 @@
 using namespace std;
 
 int main() {
-    vector<int> arr = {10, 2, 33, 50, 60, 80};
-    int n = arr.size();
+    vector<int> a = {10, 2, 33, 50, 60, 80};
+    int size = a.size(), ans = 1;
 
-    vector<int> dp(n, 1);
+    vector<int> dp(size, 1);
 
-    
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (arr[i] > arr[j]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-    }
+    for (int i = 1; i < size; i++)
+        for (int j = 0; j < i; j++)
+               
+                if (a[i] > a[j])
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    
 
-    
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        ans = max(ans, dp[i]);
-    }
+    for (int x : dp)
+        ans = max(ans, x);
 
-    cout << "Length of LIS = " << ans;
-
-    return 0;
+    cout << "LIS = " << ans;
 }
